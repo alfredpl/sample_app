d1bb6fdc322df35197f66634be9b3d55beb6f71a
alfred <alfred@arrivusystems.com>
2013-06-26 10:25:24 +0530

    Make a basic User Model(including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	it "should have the right links on the layout" do
+	visit root_path
+	click_link "About"
+	page.should have_selector 'title', text: full_title('About Us')
+	click_link "Help"
+	page.should have_selector 'title', text: full_title('Help')
+	click_link "Contact"
+	page.should have_selector 'title', text: full_title('Contact Us')
+	click_link "Home"
+	click_link "Sign up Now!"
+	page.should #
+	click_link "Sample app"
+	page.should #
+end
 
 subject { page }
 
@@ -9,7 +23,7 @@ describe "Home page" do
 before { visit root_path }
 it { should have_selector('h1',text:'Sample App') }
 it { should have_selector('title', text: full_title('')) }
-it { should_not have_selector 'title', text: '| Home' }
+it { should have_selector 'title', text: 'Home' }
 end
 
 describe "Help page" do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,8 +4,7 @@ describe "UserPages" do
   subject { page }
 describe "signup page" do
 before { visit signup_path }
-it { should have_selector('h1',
-text: 'Sign up') }
+it { should have_selector('h1',text: 'Sign up') }
 it { should have_selector('title', text: full_title('Sign up')) }
 end
 end
