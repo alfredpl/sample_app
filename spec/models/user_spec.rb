e8310c925df5d9d13238c029b713c70f573efc60
alfred <alfred@arrivusystems.com>
2013-06-28 12:50:18 +0530

    Sign-in-out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,12 +12,17 @@
 require 'spec_helper'
 
 describe User do
+	before do
+ 		@user=User.new(name: "alfred",email: "user@gmail.com",
+ 		password:"alfred123",password_confirmation:"alfred123")
+ 	end
+
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
+	it { should respond_to(:authenticate) }
 
   #pending "add some examples to (or delete) #{__FILE__}"
- before do
- 	@user=User.new(name: "alfred",email: "user@gmail.com",
- 		password:"alfred123",password_confirmation:"alfred123")
- end
+ 
    	subject { @user }
 
   	it { should respond_to(:name) }
@@ -100,6 +105,11 @@ describe User do
 	describe "with a password that's too short" do
 		before { @user.password=@user.password_confirmation="a" * 5 }
 		it { should be_invalid}
+	end
+
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
 	end
 end
 
